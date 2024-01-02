d370a5cf4afe8a663f237f7dd10bdaa6013270da
jvnaidu07 <jvnaidu07@gmail.com>
2024-01-02 21:12:31 +0530

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  // These settings apply everywhere unless overridden
+  defaultCommandTimeout: 5000,
+  viewportWidth: 1000,
+  viewportHeight: 600,
+  // Viewport settings overridden for component tests
+  // Command timeout overridden for E2E tests
+  e2e: {
+    defaultCommandTimeout: 10000,
+  },
+})
+//Cypress.config('pageLoadTimeout', 100000)
+
---
+++ cypress/e2e/api.cy.js
@@ -0,0 +1,26 @@
+/// <reference types = 'cypress'/>
+
+
+describe("cypress api", ()=>{
+    before(function(){
+        cy.fixture("example").then(function(data){
+            this.data = data;
+            globalThis.data=data
+        })
+    })
+    it("", ()=>{
+        cy.visit("https://www.google.com/");
+        cy.get("#APjFqb").type(data.name)
+        // cy.intercept("GET", "https://pagead2.googlesyndication.com/getconfig/sodar?sv=200&tid=gda&tv=r20231207&st=env").as('waitForPageLoad')
+        // cy.get("#primary-menu li a").eq(2).click({force:true})
+        // cy.wait("@waitForPageLoad")
+    })
+    it("", ()=>{
+        cy.visit("https://www.google.com/");
+        cy.get("#APjFqb").type(data.name)
+        // cy.intercept("GET", "https://pagead2.googlesyndication.com/getconfig/sodar?sv=200&tid=gda&tv=r20231207&st=env").as('waitForPageLoad')
+        // cy.get("#primary-menu li a").eq(2).click({force:true})
+        // cy.wait("@waitForPageLoad")
+    })
+})
+
