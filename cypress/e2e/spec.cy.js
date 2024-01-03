84efe3741a2b339608d5a35ebb24f94f5f157750
jvnaidu07 <jvnaidu07@gmail.com>
2024-01-04 00:45:18 +0530

    save changesss

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,14 +2,5 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
   
 })
