96bc1cf0ba6cadd75904a57c65821c65cecf0365
jvnaidu07 <jvnaidu07@gmail.com>
2024-01-10 19:10:19 +0530

    2324

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -29,12 +29,6 @@ describe("cypress api", ()=>{
         cy.url().should('eq', 'https://www.google.com/')
 
     });
-    it("url assertions", ()=>{
-        cy.visit("https://www.google.com/");
-        cy.url().should('include', 'google')
-        cy.url().should('contain', 'google')
-        cy.url().should('eq', 'https://www.google.com/')
-
-    })
+    
 })
 
