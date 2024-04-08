0b0713ea5e2a2a0201050b8b7ba638ce2fb9565a
jvnaidu07 <jvnaidu07@gmail.com>
2024-04-08 23:41:43 +0530

    save latest changes

--- cypress/e2e/Iframes.cy.js
+++ cypress/e2e/Iframes.cy.js
@@ -6,7 +6,7 @@ describe('cypress iframes', ()=>{
       
         cy.getIframe().first().find("input[type='text']").type("welcome to iframes").should('have.value', 'welcome to iframes')
     });
-    it("iframes",()=>{
+    it.only("iframes",()=>{
         cy.visit("https://demo.automationtesting.in/Frames.html")
         cy.frameLoaded('#singleframe')
         cy.iframe("#singleframe").first().find("input[type='text']").type("welcome to iframes").should('have.value', 'welcome to iframes')
