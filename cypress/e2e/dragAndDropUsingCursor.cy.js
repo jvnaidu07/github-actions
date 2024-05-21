bd79da74a57e2edd9caf331722181938c5ce1859
jvnaidu07 <jvnaidu07@gmail.com>
2024-05-21 22:46:35 +0530

    push new drag and drop spec

--- cypress/e2e/dragAndDropUsingCursor.cy.js
+++ cypress/e2e/dragAndDropUsingCursor.cy.js
@@ -1,7 +1,7 @@
 /// <reference types = "cypress"/>
 
 describe("drag and drop", ()=>{
-    it.skip("move items from one place to another place", ()=>{
+    it("move items from one place to another place", ()=>{
         const dataTransfer = new DataTransfer();
         cy.visit(Cypress.env("dragAndDrop"))
         cy.get("#products div ul li").should('have.length', 8);
