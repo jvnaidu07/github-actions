fd345700a4f0d44d8309a545ef42ff8092fa928b
jvnaidu07 <jvnaidu07@gmail.com>
2024-05-23 02:38:21 +0530

    aedfw

--- cypress/locators/selectors.js
+++ cypress/locators/selectors.js
@@ -1,18 +1,18 @@
 class storeLocators {
     trigger(index) {
-        return cy.get("#products div ul li").eq(index)
+        return cy.get("#products div ul li", {timeout: 60000}).eq(index)
     }
     triggerMouseDown() {
         return this.trigger(1)
     }
     placeholder(index) {
-        return cy.get(".placeholder").eq(index)
+        return cy.get(".placeholder", {timeout: 60000}).eq(index)
     }
     draggable() {
-        return cy.get("span[draggable='true']")
+        return cy.get("span[draggable='true']", {timeout: 60000})
     }
     dropZone() {
-        return cy.get("#mydropzone")
+        return cy.get("#mydropzone", {timeout: 60000})
     }
     angular() {
         return cy.get("#angular", { timeout: 60000 })
