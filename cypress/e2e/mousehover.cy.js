2ebbf2c76a20660b65d8e5285c2a933d7bc97040
jvnaidu07 <jvnaidu07@gmail.com>
2024-05-23 00:06:21 +0530

    push latest mouseHovers

--- cypress/e2e/mousehover.cy.js
+++ cypress/e2e/mousehover.cy.js
@@ -1,7 +1,7 @@
 /// <reference types = "Cypress"/>
 
 describe("perform mouse hover actions", () => {
-    it.only("", () => {
+    it("", () => {
         cy.visit(Cypress.env("mouseHover"));
         cy.contains("Mouse Hover DropDown").realHover;
         cy.contains("Link 1").click({ force: true });
@@ -20,4 +20,4 @@ describe("perform mouse hover actions", () => {
         cy.get("select[id='country']").select('India').should('have.value', 'India')
         cy.get("select[id='city']").select('Delhi').should('have.value', 'Delhi')        
     })
-})
+});
