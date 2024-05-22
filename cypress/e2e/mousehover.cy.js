dce9abe7dc6213fe5b58eaed2056d6621a670d9f
jvnaidu07 <jvnaidu07@gmail.com>
2024-05-22 23:43:07 +0530

    save latest changes

--- cypress/e2e/mousehover.cy.js
+++ cypress/e2e/mousehover.cy.js
@@ -1,9 +1,9 @@
 /// <reference types = "Cypress"/>
 
 describe("perform mouse hover actions", () => {
-    it("", () => {
+    it.only("", () => {
         cy.visit(Cypress.env("mouseHover"));
-        cy.contains("Mouse Hover DropDown").realHover();
+        cy.contains("Mouse Hover DropDown").realHover;
         cy.contains("Link 1").click({ force: true });
         cy.get(".modal-content").then(t => {
             expect(t).to.contain("You click on Dropdown hover Option")
