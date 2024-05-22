d57da40172f75f2644492f5dab7b79f082f58f83
jvnaidu07 <jvnaidu07@gmail.com>
2024-05-23 00:38:48 +0530

    push latest mouseHovers

--- cypress/e2e/locators.js
+++ cypress/e2e/locators.js
@@ -8,5 +8,28 @@ class getLocator {
     findSearchBar() {
         return cy.get("input[type='text']")
     }
+    nameField() {
+        return cy.get("#registration-form input", {timeout: 60000})
+    }
+    addressField() {
+        return cy.get("#registration-form input").eq(1)
+    }
+    checkcheckboxes() {
+        return cy.get("input[type='checkbox']")
+    }
+    radioButtonCheckgender() {
+        return cy.get("input[type='radio']")
+    }
+    dateOfBirthField() {
+        return cy.get("#registration-form input")
+    }
+    selectCountry() {
+        return cy.get("select[id='country']")
+    }
+    selectCity() {
+        return cy.get("select[id='city']")
+    }
+
+
 }
 export default getLocator
