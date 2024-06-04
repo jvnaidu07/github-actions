f86e5444b1ccba8170952857e34242a2e76eb33e
jvnaidu07 <jvnaidu07@gmail.com>
2024-06-05 00:01:08 +0530

    save latest changes of grid table

--- cypress/e2e/gridtable.cy.js
+++ cypress/e2e/gridtable.cy.js
@@ -12,9 +12,12 @@ describe("Validate grid table", () =>{
         "Name", "Project", "Priority", "Status", "Assigned To", "Due Date", "Effectiveness"
     ]
 
-    const updateval = (titleName) => {
+    const updateval = (titleName, project,priority,status,assignTo) => {
         cy.get("div[data-column= 'name']").contains(titleName).should('have.text', titleName);
-
+        cy.get("div[data-column='project']").contains(project).should('have.text', project)
+        cy.get("div[data-column='priority']").contains(priority).should('have.text', priority);
+        cy.get("div[data-column='status']").contains(status).should('have.text', status)
+        cy.get("div[data-column='assignedTo']").contains(assignTo).should('have.text', assignTo)
     }
 
 
@@ -27,7 +30,7 @@ describe("Validate grid table", () =>{
             cy.wrap($el).should('have.text', `${tableHeaders[index]}`)
         })
         tableContentName.forEach(val =>{
-            updateval(val.titleName)
+            updateval(val.titleName, val.project, val.priority, val.status, val.assignTo);
 
         })
     })
