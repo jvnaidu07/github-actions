2607afc8686066bfa7c74121c8debd39b877a176
jvnaidu07 <jvnaidu07@gmail.com>
2024-06-04 22:07:09 +0530

    save latest grid spec

---
+++ cypress/e2e/gridTableObj.js
@@ -0,0 +1,25 @@
+module.exports.tableContentName = 
+[
+    {titleName: "Name"},
+    {titleName: "Market Analysis"},
+    {titleName: "Product Design"},
+    {titleName: "Manufacturing Setup"},
+    {titleName:"Quality Assurance"},
+    {titleName: "Marketing Strategy"},
+    {titleName: "Product Launch"},
+    {titleName: "Follow-up Research"},
+    {titleName: "Final Report"},
+    {titleName: "Project Evaluation"},
+    {titleName: "Budgeting"},
+    {titleName: "Prototyping"},
+    {titleName: "Customer Feedback"},
+    {titleName: "Risk Assessment"},
+    {titleName: "Production Planning"},
+    {titleName: "Supply Chain Setup"},
+    {titleName: "Inventory Management"},
+    {titleName: "Quality Control"},
+    {titleName: "Sales Forecast"},
+    {titleName: "Pricing Strategy"}, 
+    {titleName: "Distribution Planning"}
+
+]
